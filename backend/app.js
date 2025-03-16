8b6bfdc1a633f55900c8e0a2adc34decce683f42
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2025-03-16 09:44:13 +0530

    trying again

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
   next();
 });
+app.use(express.static('public'));
 
 // ✅ OPTIONS request ko handle karna zaroori hai (CORS preflight issues prevent karega)
 app.options('*', cors());
