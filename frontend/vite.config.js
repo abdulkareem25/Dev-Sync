3b0346fb301acaec9bc0806fc20e646a1620bf21
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2025-03-16 08:27:42 +0530

    path added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// Function to inject headers after build
-function injectHeaders() {
-  return {
-    name: 'inject-headers',
-    closeBundle() {
-      const distPath = path.resolve(__dirname, 'dist', '_headers');
-      const headersContent = `
-/*
-  Cross-Origin-Opener-Policy: same-origin
-  Cross-Origin-Embedder-Policy: require-corp
-  Access-Control-Allow-Origin: *
-`;
-      fs.writeFileSync(distPath, headersContent);
-    }
-  };
-}
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    injectHeaders() // Plugin to add headers after build
-  ]
-});
+    tailwindcss()
+  ],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp"
+    },
+    cors: true
+  },
+  build: {
+    target: "esnext"
+  }
+})
